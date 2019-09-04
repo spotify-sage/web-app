5f5e1b3480154b01544e0d2fa5060c7c3b115974
David Paiva <pillgp@icloud.com>
2019-09-04 15:34:59 +0100

    fuck cors

--- src/Server.js
+++ src/Server.js
@@ -18,10 +18,6 @@ module.exports = class Server {
 
   	this.app.use(express.json()) // Express's JSON parser
     this.app.set('port', port)
-    this.app.use((req, res, next) => {
-      res.setHeader('Access-Control-Allow-Origin', 'http://192.168.8.101:3000')
-      next()
-    })
     // this.app.use(express.static(path.join(__dirname, 'web')))
 
     this.app.listen(port, () => {
